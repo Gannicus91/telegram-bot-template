8f819958ce8e04535cf5eba4ac3ca7e0d6b7086d
Maxim Givanyan <givanyan99@yandex-team.ru>
2022-10-16 19:56:41 +0600

    project setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf(String(process.env.BOT_TOKEN));
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+void bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
